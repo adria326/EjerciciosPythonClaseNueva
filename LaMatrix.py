d7654e0394d4a95bc549126cc2771a4ab114b073
Chantal Peñafiel <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-07-16 20:12:20 -0500

    clase nueva

---
+++ LaMatrix.py
@@ -0,0 +1,10 @@
+#Escribe un prograna que solicite al usuario
+#que ingrese su nombre. el nombre se debe alamcenar
+#en una variable llamada nombre.
+#a continuacion se debe mostrar en pantalla
+#el texto "ahora estas en la matrix, [usuario]", donde
+# "[usuario]" se reemnplaza por el nombre que
+#el usuario haya ingresado.
+
+nombre = input("escribe tu nombre: ")
+print("ahora estas en la matrix, " + nombre)
